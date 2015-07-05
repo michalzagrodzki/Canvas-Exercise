2e91ac879848269592a0505619b64d3246aff06e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-07-05 21:59:23 +0200

    Create function for changing color in each interval. Refactor code and remove duplicate function. Add comments.

--- app/js/canvas02.js
+++ app/js/canvas02.js
@@ -7,11 +7,11 @@ var lines = canvas.getContext('2d');
 // properties related to function drawTilePattern
 var horizontalTileNumber = 3;
 var verticalTileNumber = 20;
-var tileChangeSpeed = 100;
+var tileChangeSpeed = 1500;
 
 // properties related to functions gridHorizontal and gridVertical
 var gridLineColor = 'rgb(205, 205, 205)';
-var gridLineWidth = 2;
+var gridLineWidth = 1;
 
 
 // function for drawing grid of lines - goes through horizontal lines and then through vertical lines
@@ -39,34 +39,38 @@ gridVertical = function(){
   }
 };
 
+// function for setting hue
+var randomHue = function(){
+  return Math.floor((Math.random() * 359 ));
+};
+
 // function for drawing rectangle with random color
 // 1. clear canvas from previous state
-// 2. set random hue for tiles
+// 2. set one random hue value for tiles - this creates uniform color for one interval
 // 2. loop through all places on display
-// 3. set random saturation and light and attach to variable color
+// 3. in each loop set random saturation and light for one tile - this creates variance
 // 4. draw rectangle, with height and width specified from global properties
 drawTilePattern = function(){
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // function for setting hue
-  randomHue = function () {
-    return Math.floor((Math.random() + 200 ));
-  };
+  // variable for parsing hue
+  // note: this variable uses outside function which sets one value for whole loop
+  var hue = randomHue();
 
   for(x = 0; x < horizontalTileNumber; x++)for (y = 0; y < verticalTileNumber; y++) {
 
     // function for setting saturation
-    function randomSaturation() {
-      return Math.floor((Math.random() * 20) + 50);
-    }
+    randomSaturation = function () {
+      return Math.floor((Math.random() * 20) + 20);
+    };
 
     // function for setting lightness
-    function randomLight() {
+    randomLight = function () {
       return Math.floor((Math.random() * 50) + 20);
-    }
+    };
 
     // setting variable for color
-    var color = "hsl(" + randomHue() + ", " + randomSaturation() + "%, " + randomLight() + "% )";
+    var color = "hsl(" + hue + ", " + randomSaturation() + "%, " + randomLight() + "% )";
 
     // draw rectangle
     context.fillStyle = color;
