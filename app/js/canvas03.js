9a4acf1d6ac9604114fdfcc1cb2feb865d118911
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-07-08 14:26:18 +0200

    Implement function for canvas03 - loop going through whole hue spectrum.

--- app/js/canvas03.js
+++ app/js/canvas03.js
@@ -7,12 +7,40 @@ var lines = canvas.getContext('2d');
 // properties related to function drawTilePattern
 var horizontalTileNumber = 3;
 var verticalTileNumber = 20;
-var tileChangeSpeed = 1500;
+var tileChangeSpeed = 100;
 
 // properties related to functions gridHorizontal and gridVertical
 var gridLineColor = 'rgb(205, 205, 205)';
 var gridLineWidth = 1;
 
+// properties related to function incrementHue
+var incrementValue = 5;
+
+// ////////////////////// functions related to setting color /////////////////////// //
+
+// function for setting hue
+var randomHue = function(){
+  return Math.floor((Math.random() * 359 ));
+};
+
+// property for storing hue value
+// 1. in first loop value of variable is set by function randomHue()
+// 2. in second and following loops value of variable is incremented by function incrementHue()
+var hue = randomHue();
+
+// function for incrementing value of hue
+// 1. checks if hue is below 360
+// 2. increments by value specified in incrementValue
+// note: conditional is set for avoiding looping into too big numbers
+var incrementHue = function (){
+  if (hue <= 359) {
+    hue += incrementValue;
+  } else {
+    hue = 0;
+  }
+};
+
+// ////////////////////// functions related to drawing elements on screen /////////////////////// //
 
 // function for drawing grid of lines - goes through horizontal lines and then through vertical lines
 // 1. loop through all places on display
@@ -39,24 +67,15 @@ gridVertical = function(){
   }
 };
 
-// function for setting hue
-var randomHue = function(){
-  return Math.floor((Math.random() * 359 ));
-};
-
 // function for drawing rectangle with random color
 // 1. clear canvas from previous state
-// 2. set one random hue value for tiles - this creates uniform color for one interval
 // 2. loop through all places on display
 // 3. in each loop set random saturation and light for one tile - this creates variance
-// 4. draw rectangle, with height and width specified from global properties
+// 4. set hue value from outside variable - prevents from influence of loop
+// 5. draw rectangle, with height and width specified from global properties
 drawTilePattern = function(){
   // clear canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
-
-  // variable for parsing hue
-  // note: this variable uses outside function which sets one value for whole loop
-  var hue = randomHue();
 
   for(x = 0; x < horizontalTileNumber; x++)for (y = 0; y < verticalTileNumber; y++) {
 
@@ -77,9 +96,13 @@ drawTilePattern = function(){
     context.fillStyle = color;
     context.fillRect(( x * (canvas.width / horizontalTileNumber) ), ( y * (canvas.height / verticalTileNumber) ), (canvas.width / horizontalTileNumber), (canvas.height / verticalTileNumber));
   }
+
+  return hue;
+
 };
 
 // executing functions
 window.setInterval(drawTilePattern, tileChangeSpeed);
 window.setInterval(gridHorizontal, tileChangeSpeed);
 window.setInterval(gridVertical, tileChangeSpeed);
+window.setInterval(incrementHue, tileChangeSpeed);
