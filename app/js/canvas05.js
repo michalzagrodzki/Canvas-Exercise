02436100334aa8c9211e8a307aec83ea217da960
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-08-10 21:38:43 +0200

    Create temporary solution for transition.

--- app/js/canvas05.js
+++ app/js/canvas05.js
@@ -5,9 +5,9 @@ var lines = canvas.getContext('2d');
 
 // properties of visible elements
 // properties related to function drawTilePattern
-var horizontalTileNumber = 10;
-var verticalTileNumber = 10;
-var tileChangeSpeed = 2000;
+var horizontalTileNumber = 4;
+var verticalTileNumber = 4;
+var tileChangeSpeed = 500;
 
 var saturationScatter = 0;
 var saturationShift = 20;
@@ -15,12 +15,27 @@ var saturationShift = 20;
 var lightnessScatter = 5;
 var lightnessShift = 40;
 
+// properties of transition between colors
+var balanceFade = 0;
+
+
 // properties related to functions gridHorizontal and gridVertical
 var gridLineColor = 'rgb(50, 50, 50)';
 var gridLineWidth = 1;
 
-// properties related to function incrementHue
+// ////////////// functions related to blending colors /////////////////// //
 
+function blendColors(h1,s1,l1,h2,s2,l2,balance) {
+
+  var bal = Math.min(Math.max(balance,0),1);
+  var nbal = 1-bal;
+  return {
+    h : Math.floor(h1*nbal + h2*bal),
+    s : Math.floor(s1*nbal + s2*bal),
+    l : Math.floor(l1*nbal + l2*bal)
+  };
+
+}
 
 // ////////////// functions related to setting arrays for colors /////////////////// //
 
@@ -64,17 +79,18 @@ gridVertical = function(){
   }
 };
 
-// function for drawing rectangle with random color
+// function for drawing tiles  with random color
 
 drawTilePattern = function(){
   // clear canvas
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  for(x = 0; x < horizontalTileNumber; x++){
+  // loop populating firstArrayOfColors and drawing tiles
+  for(var x = 0; x < horizontalTileNumber; x++){
 
     firstArrayOfColors[x] = new Array(verticalTileNumber);
 
-    for (y = 0; y < verticalTileNumber; y++) {
+    for (var y = 0; y < verticalTileNumber; y++) {
 
       // function for setting saturation
       randomSaturation = function () {
@@ -87,22 +103,63 @@ drawTilePattern = function(){
       };
 
       // setting variable for color
-      firstArrayOfColors[x][y] = "hsl(" + firstHue + ", " + randomSaturation() + "%, " + randomLight() + "% )";
+      firstArrayOfColors[x][y] = "hsl( " + firstHue + ", " + randomSaturation() + "%, " + randomLight() + "% )";
 
-      // draw rectangle
+      // draw tile
       context.fillStyle = firstArrayOfColors[x][y];
       context.fillRect(( x * (canvas.width / horizontalTileNumber) ), ( y * (canvas.height / verticalTileNumber) ), (canvas.width / horizontalTileNumber), (canvas.height / verticalTileNumber));
 
     }
+
   }
 
-  //return firstArrayOfColors;
+  // loop populating secondArrayOfColors
+  for(var x = 0; x < horizontalTileNumber; x++){
 
+    secondArrayOfColors[x] = new Array(verticalTileNumber);
+
+    for (var y = 0; y < verticalTileNumber; y++) {
+
+      // function for setting saturation
+      randomSaturation = function () {
+        return Math.floor((Math.random() * saturationScatter) + saturationShift);
+      };
+
+      // function for setting lightness
+      randomLight = function () {
+        return Math.floor((Math.random() * lightnessScatter) + lightnessShift);
+      };
+
+      // setting variable for color
+      secondArrayOfColors[x][y] = "hsl( " + secondHue + ", " + randomSaturation() + "%, " + randomLight() + "% )";
+
+    }
+
+  }
+
+  // transition between first and second array
+   for(var x = 0; x < horizontalTileNumber; x++){
+
+    for (var y = 0; y < verticalTileNumber; y++) {
+
+      var bc = blendColors( 0, 20, 100, 100, 100, 50, balanceFade);
+      context.fillStyle = 'hsl('+ bc.h +','+ bc.s +','+ bc.l +')';
+      context.fillRect(( x * (canvas.width / horizontalTileNumber) ), ( y * (canvas.height / verticalTileNumber) ), (canvas.width / horizontalTileNumber), (canvas.height / verticalTileNumber));
+      if (balanceFade < 1) {
+        balanceFade += 0.1;
+
+      }
+
+    }
+
+  }
+  // move colors from secondArrayOfColors into firstArrayOfColors;
+  firstArrayOfColors = secondArrayOfColors;
 
 };
 
 // executing functions
-window.setInterval(drawTilePattern, tileChangeSpeed);
-//window.setInterval(gridHorizontal, tileChangeSpeed);
-//window.setInterval(gridVertical, tileChangeSpeed);
 window.setInterval(randomHue, tileChangeSpeed);
+window.setInterval(drawTilePattern, tileChangeSpeed);
+
+
